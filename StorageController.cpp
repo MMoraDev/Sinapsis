29da41ee9c357383a94eb094bf45aad21ed57185
MiguelDev001 <86728352+MiguelDev001@users.noreply.github.com>
2021-07-20 17:16:23 -0500

    Update StorageController.cpp

--- StorageController.cpp
+++ StorageController.cpp
@@ -29,20 +29,23 @@ using namespace std;
 
 /*****************************/
 // Methods
-void storageController::readFile() {
+void storageController::readFile()
+{
 
     ifstream file;
     string text;
 
     file.open("file.csv", ios::in);
     
-    if (file.fail()) {
+    if (file.fail())
+    {
         #ifdef _DEBUG
                 Console().debug("Error loading file", "There has been an error while opening the file.", __LINE__, __FILE__, Console::Message_Type::ERR);
         #endif
     }
 
-    while (!file.eof()) {
+    while (!file.eof())
+    {
         getline(file, text, ',');
         cout << text << endl;
     }

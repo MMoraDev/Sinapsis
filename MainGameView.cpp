8dbfaa073175c2940c51808a6c41f272abdfec75
NACH0SNIKE <nachosnikegum@gmail.com>
2021-08-05 23:13:21 -0500

    .

--- MainGameView.cpp
+++ MainGameView.cpp
@@ -81,22 +81,22 @@ void MainGameView::initDrawables()
 		#endif
 	}
 
-	logoScale = Vector2f((float)(this->logo->getSize().x * 0.2) / this->width, (float)(this->logo->getSize().x * 0.2) / this->width);
+	logoScale = Vector2f((float)(this->logo->getSize().x * 0.15) / this->width, (float)(this->logo->getSize().x * 0.15) / this->width);
 	franjasScale = Vector2f((float)(this->logo->getSize().x) / this->width, (float)(this->logo->getSize().x) / this->width);
 	xScale = Vector2f((float)(this->logo->getSize().x) / this->width, (float)(this->logo->getSize().x) / this->width);
 
 	// Creates the background circles for the strike's X
 	circle1->setRadius(30);
 	circle1->setFillColor(APP_COLORS().GRAY);
-	circle1->setPosition((float)(CENTER.x - 30), (float)(this->height * 6 / 10) + 20);
+	circle1->setPosition((float)(CENTER.x - 30), (float)(this->height * 6 / 10) + 150);
 
 	circle2->setRadius(30);
 	circle2->setFillColor(APP_COLORS().GRAY);
-	circle2->setPosition((float)(CENTER.x - 155), (float)(this->height * 6 / 10) + 20);
+	circle2->setPosition((float)(CENTER.x - 155), (float)(this->height * 6 / 10) + 150);
 
 	circle3->setRadius(30);
 	circle3->setFillColor(APP_COLORS().GRAY);
-	circle3->setPosition((float)(CENTER.x + 95), (float)(this->height * 6 / 10)+ 20);
+	circle3->setPosition((float)(CENTER.x + 95), (float)(this->height * 6 / 10)+ 150);
 	
 	// Convert bg.png (from image -> texture -> sprite) to draw on canvas
 	this->bg->setSmooth(true);
@@ -113,22 +113,22 @@ void MainGameView::initDrawables()
 	this->logo->setSmooth(true);
 	logoSprite->setScale(logoScale);
 	logoSprite->setTexture(*(this->logo));
-	logoSprite->setPosition((float)(this->width / 2) - (this->logo->getSize().x * (logoScale.x / 2.f)), (float)this->height * 4 / 5);
+	logoSprite->setPosition((float)(this->width / 2) - (this->logo->getSize().x * (logoScale.x / 2.f)), (float)(this->height * 4 / 5) + 50);
 
 	// Convert x.png (from image -> texture -> sprite) to draw on canvas
 	this->xS->setSmooth(true);
 
 	xSprite1->setScale(xScale);
 	xSprite1->setTexture(*(this->xS));
-	xSprite1->setPosition((float)((this->width / 2) - (this->xS->getSize().x * (xScale.x / 2.f))) - 125, (float)(this->height * 6 / 10) + 8);
+	xSprite1->setPosition((float)((this->width / 2) - (this->xS->getSize().x * (xScale.x / 2.f))) - 125, (float)(this->height * 6 / 10) + 175);
 
 	xSprite2->setScale(xScale);
 	xSprite2->setTexture(*(this->xS));
-	xSprite2->setPosition((float)((this->width / 2) - (this->xS->getSize().x * (xScale.x / 2.f))), (float)(this->height * 6 / 10) + 8);
+	xSprite2->setPosition((float)((this->width / 2) - (this->xS->getSize().x * (xScale.x / 2.f))), (float)(this->height * 6 / 10) + 175);
 
 	xSprite3->setScale(xScale);
 	xSprite3->setTexture(*(this->xS));
-	xSprite3->setPosition((float)((this->width / 2) - (this->xS->getSize().x * (xScale.x / 2.f))) + 125, (float)(this->height * 6 / 10) + 8);
+	xSprite3->setPosition((float)((this->width / 2) - (this->xS->getSize().x * (xScale.x / 2.f))) + 125, (float)(this->height * 6 / 10) + 175);
 
 	//Drawables
 	this->drawables["bg"] = bgSprite;
@@ -146,9 +146,9 @@ void MainGameView::initDrawables()
 	this->drawables["xa2"] = xSprite2;
 	this->drawables["xa3"] = xSprite3;
 	this->drawables["logoImage"] = logoSprite;
-	this->drawables["question"] = new WrappableText(this->parent, (int)this->x + (this->width / 2) - (int)(this->width * 2) / 10, 40, 55, (int)(this->width * 2) / 5, "Pregunta X", PADDING, WrappableText::Style::TITLE, WrappableText::TextAlign::CENTER, APP_COLORS().GRAY_LIGHT, APP_COLORS().PRIMARY);
-	this->drawables["qauestionsBg"] = new RoundedRectangle(this->parent, this->x + (this->width / 2) - 350, 65.5, 300, 700, BORDER_RADIUS, APP_COLORS().WHITE);
-	this->drawables["qauestionsaBg"] = new RoundedRectangle(this->parent, this->x + (this->width / 2) - 345, 70.5, 290, 690, BORDER_RADIUS, APP_COLORS().GRAY_LIGHT);
-	this->drawables["redTeam"] = new WrappableText(this->parent, (int)this->x + (this->width * 1 / 40), 0, 45, (int)(this->width * 1) / 5, "EQUIPO ROJO", PADDING, WrappableText::Style::SUBTITLE, WrappableText::TextAlign::LEFT, APP_COLORS().GRAY_LIGHT, APP_COLORS().WHITE ,true);
-	this->drawables["rblueTeam"] = new WrappableText(this->parent, (int)this->x + (this->width * 39 / 40) - (this->width * 1) / 5, 0, 45, (int)(this->width * 1) / 5, "EQUIPO AZUL", PADDING, WrappableText::Style::SUBTITLE, WrappableText::TextAlign::RIGHT, APP_COLORS().GRAY_LIGHT, APP_COLORS().WHITE, true);
+	this->drawables["question"] = new WrappableText(this->parent, (int)this->x + (this->width / 2) - (int)(this->width * 2) / 10, 65, 55, (int)(this->width * 2) / 5, "Pregunta X", PADDING, WrappableText::Style::TITLE, WrappableText::TextAlign::CENTER, APP_COLORS().GRAY_LIGHT, APP_COLORS().PRIMARY);
+	this->drawables["qauestionsBg"] = new RoundedRectangle(this->parent, this->x + (this->width / 2) - 350, 90.5, 300, 700, BORDER_RADIUS, APP_COLORS().WHITE);
+	this->drawables["qauestionsaBg"] = new RoundedRectangle(this->parent, this->x + (this->width / 2) - 345, 95.5, 290, 690, BORDER_RADIUS, APP_COLORS().GRAY_LIGHT);
+	this->drawables["redTeam"] = new WrappableText(this->parent, (int)this->x + (this->width * 1 / 40) + 10, 5, 45, (int)(this->width * 1) / 5, "EQUIPO ROJO", PADDING, WrappableText::Style::SUBTITLE, WrappableText::TextAlign::LEFT, APP_COLORS().GRAY_LIGHT, APP_COLORS().GRAY_LIGHT ,true);
+	this->drawables["rblueTeam"] = new WrappableText(this->parent, (int)this->x + (this->width * 39 / 40) - (this->width * 1) / 5 - 10, 5, 45, (int)(this->width * 1) / 5, "EQUIPO AZUL", PADDING, WrappableText::Style::SUBTITLE, WrappableText::TextAlign::RIGHT, APP_COLORS().GRAY_LIGHT, APP_COLORS().GRAY_LIGHT, true);
 };
